88fea766b8eb711871e4790c337d9e7823ddf065
MasahiKon <konnonorth@gmail.com>
2023-01-31 12:15:11 -0800

    done

--- Exercise/script.js
+++ Exercise/script.js
@@ -9,4 +9,32 @@
 
 $(document).ready(function () {
     // code goes here
+    $("#addTask").click(function() {
+        const value = $(".textBox").val();
+        console.log(value);
+        if(!value) {alert("Error: Please enter a task first"); return};
+        const task = $(`<div><span>${value}</span><span><i class="fa fa-check"></i> <i class="fa fa-trash-alt"></i></span></div>`);
+        task.css({
+            "display": "flex",
+            "justify-content": "space-between",
+            "margin": "0 15px",
+            "padding": " 10px 10px",
+            "background-color": "rgba(0, 0, 100, 0.2)"
+        });
+        task.find(".fa-check").click(function() {
+            if($(this).parent().parent().parent().attr('class') === "completed") return;
+            $(this).parent().parent().fadeOut().promise().done(function() {
+                const item = $(this).detach();
+                $(".completed").append(item);
+                item.fadeIn();
+            })
+
+        });
+        task.find(".fa-trash-alt").click(function() {
+            $(this).parent().parent().fadeOut().promise().done(function() {
+                $(this).remove();
+            })
+        });
+        $(".notCompleted").append(task)
+    })
 });
